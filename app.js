288823cd74fec15af293341ab50880c46849bef3
Jaap de Jong <jaapdejong15@gmail.com>
2018-11-26 14:37:19 +0100

    h

--- app.js
+++ app.js
@@ -4,5 +4,10 @@ var http = require("http");
 var port = process.argv[2];
 var app = express();
 
+var indexRouter = require("./routes/index");
+
 app.use(express.static(__dirname + "/public"));
 http.createServer(app).listen(port);
+
+app.get("/play", indexRouter);
+app.get("/", indexRouter);
