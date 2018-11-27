2e01921edb823f24c127764119e884d562ef66ab
Jaap de Jong <jaapdejong15@gmail.com>
2018-11-27 12:26:11 +0100

    Commented some code that gave errors for testing, added favicon & sprites for pieves, setValue now fully implemented

--- public/javascripts/serverCommunication.js
+++ public/javascripts/serverCommunication.js
@@ -3,22 +3,31 @@ var letters = new Array("A", "B", "C", "D", "E", "F", "G", "H");
 
 $("#boardTable tr td").click(function(event){
     console.log(event.target.id);
-    setValue("1", event.target.id);
-    var location = event.target.id;
+
+    //For testing purposes
+    setValue("-1", event.target.id);
+
+    /*
+    var location = event.target.id; 
     var yLoc = letters.indexOf(location.charAt(0)) + 1;
     var xLoc = parseInt(location.charAt(1));
     console.log(xLoc + " - " + yLoc);
     var move = {type:"MOVE", x:xLoc, y:yLoc};
     socket.send(JSON.stringify(move));
-
-    
+    */
 })
 
 function setValue(value, location){
-    document.getElementById(location).innerHTML = value; 
+    console.log("start of setValue method");
+    if(value == 1){
+        document.getElementById(location).style.backgroundImage = "url('/images/BlackPiece.png')"; 
+    } else if(value == -1){
+        document.getElementById(location).style.backgroundImage = "url('/images/WhitePiece.png')";
+    }
 }
 
 socket.on("message", function incoming(message) {
-    
+
 });
+
 
