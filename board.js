1faf5cabd53ac6e871c64d0ca7cc2755b044f2a2
Ivar de Bruin <ivardb@gmail.com>
2018-12-03 12:52:27 +0100

    ESLINT done

--- board.js
+++ board.js
@@ -35,59 +35,59 @@ function getValueB (board, x, y) {
     }
 }
 
-function getValueL(board, x, y){
-    if(x <= 0)return null;
-    else{
-        return board.boardArray[y][x-1];
+function getValueL (board, x, y) {
+    if (x <= 0) return null
+    else {
+        return board.boardArray[y][x - 1]
     }
 }
 
-function getValueR(board, x, y){
-    if(x >= 7)return null;
-    else{
-        return board.boardArray[y][x+1];
+function getValueR (board, x, y) {
+    if (x >= 7) return null
+    else {
+        return board.boardArray[y][x + 1]
     }
 }
 
-function getValueTL(board, x, y){
-    if(y <= 0 || x <= 0 )return null;
-    else{
-        return board.boardArray[y-1][x-1];
+function getValueTL (board, x, y) {
+    if (y <= 0 || x <= 0) return null
+    else {
+        return board.boardArray[y - 1][x - 1]
     }
 }
 
-function getValueTR(board, x, y){
-    if(y <= 0 || x >= 7 )return null;
-    else{
-        return board.boardArray[y-1][x+1];
+function getValueTR (board, x, y) {
+    if (y <= 0 || x >= 7) return null
+    else {
+        return board.boardArray[y - 1][x + 1]
     }
 }
 
-function getValueBL(board, x, y){
-    if(y >= 7 || x <= 0 )return null;
-    else{
-        return board.boardArray[y+1][x-1];
+function getValueBL (board, x, y) {
+    if (y >= 7 || x <= 0) return null
+    else {
+        return board.boardArray[y + 1][x - 1]
     }
 }
 
-function getValueBR(board, x, y){
-    if(y >= 7 || x >= 7 )return null;
-    else{
-        return board.boardArray[y+1][x+1];
+function getValueBR (board, x, y) {
+    if (y >= 7 || x >= 7) return null
+    else {
+        return board.boardArray[y + 1][x + 1]
     }
 }
 
-function getAdjacent(board, x,y){
-    ret = new Array(8);
-    ret[0] = getValueTL(board, x,y);
-    ret[1] = getValueT(board, x,y);
-    ret[2] = getValueTR(board, x, y);
-    ret[3] = getValueL(board, x, y);
-    ret[4] = getValueR(board, x, y);
-    ret[5] = getValueBL(board, x, y);
-    ret[6] = getValueB(board, x, y);
-    ret[7] = getValueBR(board, x, y);
-    return ret;
+function getAdjacent (board, x, y) {
+    let ret = new Array(8)
+    ret[0] = getValueTL(board, x, y)
+    ret[1] = getValueT(board, x, y)
+    ret[2] = getValueTR(board, x, y)
+    ret[3] = getValueL(board, x, y)
+    ret[4] = getValueR(board, x, y)
+    ret[5] = getValueBL(board, x, y)
+    ret[6] = getValueB(board, x, y)
+    ret[7] = getValueBR(board, x, y)
+    return ret
 }
 
-module.exports = board;
+module.exports = board
--- gamestate.js
+++ gamestate.js
@@ -1,80 +1,80 @@
-var game = function() {
-    this.scorePlayerA=2;
-    this.scorePlayerB=2;
-    this.state = "Starting"; //other possibilities are turnWhite, turnBlack;
-    this.validMovesBlack = [[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]];
-    this.validMovesWhite = [[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]];
-    this.nickNameA = null; //nicknames
-    this.nickNameB = null;
-    this.awaitingMove = false; //signals to the client if it is expecting a move or not
-    this.updateMoves = updateMoves;
-    this.getValidMove = getValidMove;
-    this.calculateScore = calculateScore;
-    this.canMove = canMove;
-    this.clear = clear;
+var gameState = function () {
+    this.scorePlayerA = 2
+    this.scorePlayerB = 2
+    this.state = 'Starting' // other possibilities are turnWhite, turnBlack;
+    this.validMovesBlack = [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]]
+    this.validMovesWhite = [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]]
+    this.nickNameA = null // nicknames
+    this.nickNameB = null
+    this.awaitingMove = false // signals to the client if it is expecting a move or not
+    this.updateMoves = updateMoves
+    this.getValidMove = getValidMove
+    this.calculateScore = calculateScore
+    this.canMove = canMove
+    this.clear = clear
 }
-function updateMoves(x, y, color) {
-    if(color == 1) {
-        this.validMovesBlack[y][x] = 1;
+function updateMoves (x, y, color) {
+    if (color === 1) {
+        this.validMovesBlack[y][x] = 1
     } else {
-        this.validMovesWhite[y][x] = 1;
+        this.validMovesWhite[y][x] = 1
     }
 }
 
-function getValidMove(x, y, color) {
-    if(color == -1) {
-        if(this.validMovesWhite[y][x] == 1) {
-            return true;
+function getValidMove (x, y, color) {
+    if (color === -1) {
+        if (this.validMovesWhite[y][x] === 1) {
+            return true
         }
     } else {
-        if(this.validMovesBlack[y][x] == 1) {
-            return true;
+        if (this.validMovesBlack[y][x] === 1) {
+            return true
         }
     }
-    return false;
+    return false
 }
 
-function canMove(color) {
-    var res = false;
-    if(color == -1) {
-        for(i=0;i<8;i++) {
-            if(this.validMovesWhite[i].includes(1)) {
-                res = true;
+function canMove (color) {
+    var res = false
+    if (color === -1) {
+        for (let i = 0; i < 8; i++) {
+            if (this.validMovesWhite[i].includes(1)) {
+                res = true
             }
         }
     } else {
-        for(i=0;i<8;i++) {
-            if(this.validMovesBlack[i].includes(1)) {
-                res = true;
+        for (let i = 0; i < 8; i++) {
+            if (this.validMovesBlack[i].includes(1)) {
+                res = true
             }
         }
     }
-    return res;
+    return res
 }
 
-function clear() {
-    for(i=0;i<8;i++) {
-        for(j=0;j<8;j++) {
-            this.validMovesBlack[i][j] = 0;
-            this.validMovesWhite[i][j] = 0;
+function clear () {
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            this.validMovesBlack[i][j] = 0
+            this.validMovesWhite[i][j] = 0
         }
     }
 }
 
-function calculateScore(board){
-    var scoreA=0;
-    var scoreB=0;
-    boardArray = board.boardArray;
-    for(i=0;i<8;i++) {
-        for(j=0;j<8;j++) {
-            if(boardArray[i][j] == 1) {
-                scoreB++;
-            } else if(boardArray[i][j] == -1) {
-                scoreA++;
+function calculateScore (board) {
+    var scoreA = 0
+    var scoreB = 0
+    let boardArray = board.boardArray
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            if (boardArray[i][j] === 1) {
+                scoreB++
+            } else if (boardArray[i][j] === -1) {
+                scoreA++
             }
         }
-        this.scorePlayerA = scoreA;
-        this.scorePlayerB = scoreB;
+        this.scorePlayerA = scoreA
+        this.scorePlayerB = scoreB
     }
 }
-module.exports = game;
+module.exports = gameState
