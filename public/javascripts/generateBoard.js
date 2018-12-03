7b27cacbfb4b499d9cc203db0f935c30f41539bf
Ivar de Bruin <ivardb@gmail.com>
2018-12-03 13:07:28 +0100

    ESLINT

--- public/javascripts/generateBoard.js
+++ public/javascripts/generateBoard.js
@@ -1,21 +1,19 @@
-function generateBoard(){
-    var letters = new Array("A", "B", "C", "D", "E", "F", "G", "H");
-    var board = new Array();
-    var container = document.getElementById("board");
-    var table = document.createElement("table");
-    table.id = "boardTable";
-    for(var i=0; i<8; i++){
-        var row = document.createElement("tr");
-        row.id = "row " + letters[i];
-        for(var j=1; j<=8; j++){
-            var cell = document.createElement("td");
-            var id = "" + letters[i] + j;
-            cell.id = id;
-            row.appendChild(cell);
+function generateBoard () {
+    var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+    var container = document.getElementById('board')
+    var table = document.createElement('table')
+    table.id = 'boardTable'
+    for (var i = 0; i < 8; i++) {
+        var row = document.createElement('tr')
+        row.id = 'row ' + letters[i]
+        for (var j = 1; j <= 8; j++) {
+            var cell = document.createElement('td')
+            var id = '' + letters[i] + j
+            cell.id = id
+            row.appendChild(cell)
         }
-        table.appendChild(row);
+        table.appendChild(row)
     }
-    container.appendChild(table);
-
+    container.appendChild(table)
 }
-generateBoard();
+generateBoard()
--- public/javascripts/timer.js
+++ public/javascripts/timer.js
@@ -1,18 +1,20 @@
-function timer(){
-    var startTime = Date.now();
-    setInterval(function(){
-        var timeElapsed = Date.now() - startTime;
-        var seconds = Math.floor(timeElapsed / 1000);
-        var minutes = Math.floor(seconds/60);
-        seconds -= minutes * 60;
-        var hours = Math.floor(minutes/60);
-        minutes -= hours * 60;
-        var strSeconds = seconds;
-        var strMinutes = minutes;
-        var strHours = hours;
-        if(seconds < 10)strSeconds = "0"+strSeconds;
-        if(minutes < 10)strMinutes = "0"+strMinutes;
-        if(hours < 10)strHours = "0"+strHours;
-        document.getElementById("clock").innerHTML = strHours + ":" + strMinutes + ":" + strSeconds;
-    }, 100);    
+function timer () {
+    var startTime = Date.now()
+    setInterval(function () {
+        var timeElapsed = Date.now() - startTime
+        var seconds = Math.floor(timeElapsed / 1000)
+        var minutes = Math.floor(seconds / 60)
+        seconds -= minutes * 60
+        var hours = Math.floor(minutes / 60)
+        minutes -= hours * 60
+        var strSeconds = seconds
+        var strMinutes = minutes
+        var strHours = hours
+        if (seconds < 10)strSeconds = '0' + strSeconds
+        if (minutes < 10)strMinutes = '0' + strMinutes
+        if (hours < 10)strHours = '0' + strHours
+        document.getElementById('clock').innerHTML = strHours + ':' + strMinutes + ':' + strSeconds
+    }, 100)
 }
+
+module.exports = timer
