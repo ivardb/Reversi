32947e14de863740d294e1d631960efac25fabac
Ivar de Bruin <ivardb@gmail.com>
2018-12-03 14:09:02 +0100

    ESLINT

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,14 @@
-var express = require("express");
-var router = express.Router();
-
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
-router.get("/", function (req, res) {
-    res.sendFile("splash.html", {root: "./public"});
-});
+router.get('/', function (req, res) {
+    res.sendFile('splash.html', { root: './public' })
+})
 
 /* Pressing the 'PLAY' button, returns this page */
-router.get("/play", function(req, res) {
-    res.sendFile("game.html", {root: "./public"});
-});
+router.get('/play', function (req, res) {
+    res.sendFile('game.html', { root: './public' })
+})
 
-module.exports = router;
+module.exports = router
